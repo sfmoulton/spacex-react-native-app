3f2527f96afee0d077f8fe9994c2e9240b8e2bf4
sfmoulton <stephen.m@raytheon.co.uk>
2022-07-11 21:10:47 +0100

    Linked fonts from assets, added styling to list item

--- src/components/ListButton.tsx
+++ src/components/ListButton.tsx
@@ -7,7 +7,7 @@ import {
   Image,
   Button,
 } from "react-native";
-import { DesignColours } from "../assets/colours/colours";
+import { DesignColours } from "../../assets/colours/colours";
 
 interface Props {
   buttonName: string;
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -10,15 +10,20 @@ interface Props {
 
 const ListItem = (props: Props) => {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>#{props.flightNumber}</Text>
-      <Text>This is the {props.missionName} item!</Text>
-      <Text>{props.launchDate}</Text>
-      <Text>{props.rocketName}</Text>
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <Text style={styles.boldText}>#{props.flightNumber}</Text>
+      <Text style={styles.boldText}>{props.missionName}</Text>
+      <View>
+        <Text style={styles.text}>{props.launchDate}</Text>
+        <Text style={styles.boldText}>{props.rocketName}</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  boldText: { fontFamily: "BrandonGrotesque-Medium", fontSize: 18 },
+  text: {fontFamily: "BrandonGrotesque-Regular", fontSize: 12}
+});
 
 export default ListItem;
