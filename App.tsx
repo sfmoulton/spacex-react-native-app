e75d19b7dbaa859410ad3c5112d5fec6ec9c836d
Stephen Moulton <stephen@stephens-mbp.home>
2022-07-12 15:05:14 +0100

    Removed unnecessary code in App

--- App.tsx
+++ App.tsx
@@ -1,121 +1,25 @@
 import React from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-
-import { getAllSpaceXLaunches } from "./src/api/space-x-api";
 import LaunchListScreen from "./src/screens/LaunchListScreen";
 import Header from "./src/components/Header";
 
-// const Section: React.FC<
-//   PropsWithChildren<{
-//     title: string;
-//   }>
-// > = ({children, title}) => {
-//   const isDarkMode = useColorScheme() === "dark";
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
 const App = () => {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  // here we will want to render the list screen - and add comments about here is where the nav would go?
-
-  // useEffect(() => {
-  //   const fetchAllLaunches = async () => {
-  //     const result = await getAllSpaceXLaunches();
-  //     console.log(result.data[0].launch_year);
-  //   }
-  //   fetchAllLaunches();
-  // }, []);
+  // This is where we would add in our navigation - as we need a single screen I chose not to focus on this
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        {/* We are seeing a virtualised list warning, as we are using a ScrollView and FlatList together with the same orientation */}
+        {/* Next step is to look at solutions to this, as on some devices the whole screen will not be visible - */}
+        {/* So we want the user to be able to scroll both the list and the screen itself */}
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          {/* <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />{" "}
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section> */}
-          {/* <LearnMoreLinks /> */}
+        <View>
           <LaunchListScreen />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-});
 
 export default App;
